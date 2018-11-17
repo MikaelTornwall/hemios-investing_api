a682d216602a3ff6aab37c2a3299b47a11493dd7
Mikael Törnwall <mikael.tornwall@aalto.fi>
2018-11-17 17:12:51 +0200

    routes to cashflow, balace sheet and income statement

--- controllers/data.js
+++ controllers/data.js
@@ -25,4 +25,58 @@ dataRouter.get('/:id', async (req, res) => {
   }
 })
 
+dataRouter.get('/:id/cashflowStatement/', async (req, res) => {
+  try {
+    const id = req.params.id
+    const data = await Data.findById(id)
+    console.log(data)
+
+    if (data) {
+      const cashflowStatement = await data.cashflowStatementHistoryQuarterly
+      res.json(cashflowStatement)
+    } else {
+      res.status(404).end()
+    }
+
+  } catch (error) {
+    res.status(400).send({ error: 'malformatted' })
+  }
+})
+
+dataRouter.get('/:id/balanceSheet/', async (req, res) => {
+  try {
+    const id = req.params.id
+    const data = await Data.findById(id)
+    console.log(data)
+
+    if (data) {
+      const balanceSheet = await data.balanceSheetHistoryQuarterly
+      res.json(balanceSheet)
+    } else {
+      res.status(404).end()
+    }
+
+  } catch (error) {
+    res.status(400).send({ error: 'malformatted' })
+  }
+})
+
+dataRouter.get('/:id/incomeStatement/', async (req, res) => {
+  try {
+    const id = req.params.id
+    const data = await Data.findById(id)
+    console.log(data)
+
+    if (data) {
+      const incomeStatement = await data.incomeStatementHistoryQuarterly
+      res.json(incomeStatement)
+    } else {
+      res.status(404).end()
+    }
+
+  } catch (error) {
+    res.status(400).send({ error: 'malformatted' })
+  }
+})
+
 module.exports = dataRouter
--- models/companyData.js
+++ models/companyData.js
@@ -27,3 +27,27 @@ dataSchema.statics.format = (data) => {
 const Data = mongoose.model('Data', dataSchema)
 
 module.exports = Data
+
+// const dataSchema = new mongoose.Schema({
+//   name: String,
+//   id: String,
+//   cashflowStatementHistoryQuarterly: {
+//   },
+//   balanceSheetHistoryQuarterly: {
+//   },
+//   incomeStatementHistoryQuarterly: {
+//   }
+// },
+// {
+//   collection: 'AAPL'
+// })
+//
+// dataSchema.statics.format = (data) => {
+//   return {
+//     name: data.name,
+//     id: data._id,
+//     cashflowStatementHistoryQuarterly: data.cashflowStatementHistoryQuarterly,
+//     balanceSheetHistoryQuarterly: data.balanceSheetHistoryQuarterly,
+//     incomeStatementHistoryQuarterly: data.incomeStatementHistoryQuarterly
+//   }
+// }
